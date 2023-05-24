cdf88636b7527dd3a2c825857546d80f0052d341
Sávio Antônio Castro de Sousa <84689809+Savio-Castro@users.noreply.github.com>
2023-05-24 13:41:40 -0300

    Delete modelo-de-caixa_arquivos directory

--- modelo-de-caixa_arquivos/course-taking-server-side.cadc71a15e5668e2e2f9.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["course-taking-server-side","browse-server-side","course-manage-announcements-server-side","lab-manage-server-side","lab-taking-server-side","lecture-preview-server-side","messaging-server-side","organization-manage-courses-server-side","organization-manage-settings-custom-error-message-server-side"],[]]);
-//# sourceMappingURL=course-taking-server-side.cadc71a15e5668e2e2f9.js.map
